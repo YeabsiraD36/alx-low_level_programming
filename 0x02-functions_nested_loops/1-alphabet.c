2a5a9709d4575ddd540d468d6b199d4442f150f6
Yeabsira Daniel <yeabsiradaniel36@gmail.com>
2022-09-12 23:00:58 -0700

    1-alphabet added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * print_alphabet - Entry point
+ * Description: a program display alphabet in lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
