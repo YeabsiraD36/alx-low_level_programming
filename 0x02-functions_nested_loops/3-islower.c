83339638ae85a036dd06765689813596adce2ec8
Yeabsira Daniel <yeabsiradaniel36@gmail.com>
2022-09-13 00:38:27 -0700

    3-islower added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _islower - is a function that identify lowercase letters
+ * @c: is test subject
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
