ae3d01f632dfd7e2b574b9c217ada177eead7373
Yeabsira Daniel <yeabsiradaniel36@gmail.com>
2022-09-13 01:22:11 -0700

    abs added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - function that puts integer in absoulute value
+ * @n: test subject
+ * Return: Void.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+	return (n);
+	}
+}
