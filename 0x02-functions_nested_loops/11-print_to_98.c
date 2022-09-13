063077d2c2e5c24119dfdc1b00fd7aed47d5937d
Yeabsira Daniel <yeabsiradaniel36@gmail.com>
2022-09-13 02:45:04 -0700

    print to 98 added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main"
+/**
+ * print_to_98 - listes upto 98
+ * @n: number entered
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d/n", i);
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d/n", i);
+			}
+		}
+	}
+}
+
+
