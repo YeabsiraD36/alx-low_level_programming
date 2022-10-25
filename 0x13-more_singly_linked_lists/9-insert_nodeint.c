fede816e2b18b11a41a231ad9be79ecdd7248ce0
Yeabsira Daniel <yeabsiradaniel36@gmail.com>
2022-10-25 07:15:55 -0700

    Final commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	if (idx == 0)
 	{
-		new->next = current->next;
+		new->next = *head;
 		*head = new;
 	}
 	else
